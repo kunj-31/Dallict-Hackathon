bfd967ba9cd8868bc9fa837e34577427c7b7f907
kunj-31 <kunjdesai487@gmail.com>
2025-04-11 21:11:52 +0530

    part 2:delete

--- src/components/ui/Card.js
+++
@@ -1,8 +0,0 @@
-// src/components/ui/Card.js
-export default function Card({ children, className = '' }) {
-  return (
-    <div className={`bg-white rounded-lg shadow-md ${className}`}>
-      {children}
-    </div>
-  );
-}
--- src/components/ui/Label.js
+++
@@ -1,8 +0,0 @@
-// src/components/ui/Label.js
-export default function Label({ htmlFor, children }) {
-  return (
-    <label htmlFor={htmlFor} className="block font-semibold mb-1">
-      {children}
-    </label>
-  );
-}
